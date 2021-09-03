5377a540e8e4578680c1007acac0edddf6522046
Jayeson Lee-Steere <jayeson@titaniumstudios.com>
2021-09-03 13:36:55 +0800

    Rename c.bat to a.bat.

--- PortLogger/Logger/a.bat
+++ PortLogger/Logger/a.bat
@@ -9,15 +9,20 @@ rem cd Logger
 rem
 rem TASM 5 should be installed to dosbox-c-device\TASM
 
+echo Assembling...
 c:\tasm\bin\tasm logger
 if not ERRORLEVEL 0 goto exit
 
+echo Linking...
 c:\tasm\bin\tlink /Tdc /3 logger.obj
 if not ERRORLEVEL 0 goto exit
 
 echo.
+echo Copying to floppy image...
 imgmount a c:\dev\img\dos622_144_boot.img -t floppy
 copy logger.com a:
 imgmount -u a
 
+echo.
+echo Exiting...
 :exit
